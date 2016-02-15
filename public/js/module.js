299a5036e5132a10c051992108be59d862346733
MaltbyRL <RLMaltby@gmail.com>
2016-02-15 10:02:54 -0800

    working on controllers

---
+++ public/js/module.js
@@ -0,0 +1,11 @@
+"use strict";
+var app = angular.module('ricksApp');
+
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  .state('home', {url: '/', template: "<h1>HOME</h1>", controller: 'homeCtrl'})
+  .state('list', {url: '/list', template: "<h1>LIST</h1>", controller: 'listCtrl'})
+
+
+  $urlRouterProvider.otherwise('/');
+});
