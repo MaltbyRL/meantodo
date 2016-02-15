a89a4b4c9fa3d6da25ecf4dd70e216fb630ee40e
MaltbyRL <RLMaltby@gmail.com>
2016-02-15 10:51:26 -0800

    still working on things

--- public/js/controllers/homeCtrl.js
+++ public/js/controllers/homeCtrl.js
@@ -4,5 +4,20 @@ var app = angular.module('someApp');
 
 app.controller('homeCtrl', function($scope, $http) {
   console.log('homeCtrl');
-  $scope.
+  var newTask = {
+    task: $scope.task,
+    dueby: $scope.dueby
+  }
+  $http.post("users", function(req, res) {
+    if(err) console.log(err)
+
+    console.log("res:", res);
+
+  })
+
+  $http.get("users", function(req, res) {
+    if(err) console.log(req, res)
+
+    console.log(res)
+  })
 });
