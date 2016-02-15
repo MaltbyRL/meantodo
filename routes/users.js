aa64939daca21004d60509e679bc2f55b87ddfe3
MaltbyRL <RLMaltby@gmail.com>
2016-02-15 10:14:17 -0800

    worked on routes

---
+++ routes/users.js
@@ -0,0 +1,20 @@
+
+Todo = require('../models/todomodel')
+
+
+
+router.post('/newTodo', function(req, res, next) {
+	console.log('req.body', req.body)
+  ref.createTodo(req.body, function(err, userData) {
+    if(err) return res.status(400).send("err",err);
+    var task = req.body.task
+    var dueby = req.body.dueby
+    var completed = req.body.completed
+    console.log("task", task)
+    console.log("Routes/req", req.body);
+ Todo.create({task: task, dueby: dueby, completed: completed}, function(err, newtodo) {
+   if(err) return res.status(400).send(err);
+      res.send();
+    });
+  });
+});
